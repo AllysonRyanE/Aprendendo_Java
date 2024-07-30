1f4814708a4fd73d871964739220e1bc72baaa05
Allyson Ryan <115114528+AllysonRyanE@users.noreply.github.com>
2024-07-30 00:00:26 -0300

    Update Sum.java

--- APS_MATH_SEQ/Sum.java
+++ APS_MATH_SEQ/Sum.java
@@ -1,16 +1,15 @@
+package Aprendendo_Java.APS_MATH_SEQ;
+
 import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        System.out.print("Primeiro número: ");
+        System.out.println("DIGITE QUATRO NÚMEROS PARA QUE SEJAM SOMADOS:");
         double n1 = reader.nextDouble();
-        System.out.print("Segundo número: ");
         double n2 = reader.nextDouble();
-        System.out.print("Terceiro número: ");
         double n3 = reader.nextDouble();
-        System.out.print("Quarto número: ");
         double n4 = reader.nextInt();
         double sum = n1 + n2 + n3 + n4;
         System.out.println("R = " + sum);
