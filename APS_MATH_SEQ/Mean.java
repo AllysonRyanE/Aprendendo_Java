4c23b4bddad127641ebf585947375baf4cc95391
Allyson Ryan <115114528+AllysonRyanE@users.noreply.github.com>
2024-07-29 23:59:14 -0300

    Update Mean.java

--- APS_MATH_SEQ/Mean.java
+++ APS_MATH_SEQ/Mean.java
@@ -1,14 +1,14 @@
+package Aprendendo_Java.APS_MATH_SEQ;
+
 import java.util.Scanner;
 
 public class Mean {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        System.out.print("Primeira atividade: ");
+        System.out.println("DIGITE TRÊS NOTAS DE ATIVIDADES: ");
         double n1 = reader.nextDouble();
-        System.out.print("Segunda atividade: ");
         double n2 = reader.nextDouble();
-        System.out.print("Terceira atividade: ");
         double n3 = reader.nextDouble();
         double mean = (n1 + n2 + n3) / 3;
         System.out.println("NOTA FINAL: " + mean);
