ec7cb59b4a6e190d18b1ce084933e809800d23d0
Allyson Ryan <115114528+AllysonRyanE@users.noreply.github.com>
2024-08-09 16:02:29 -0300

    Create OddCounter.java

---
+++ APS_REPETITION/OddCounter.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class OddCounter {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner (System.in);
+        System.out.println("Digite o primeiro número: ");
+        int n1 = reader.nextInt();
+        System.out.println("Digite o segundo número (MAIOR QUE O PRIMEIRO): ");
+        int n2 = reader.nextInt();
+        int i = n1;
+        while(i<=n2){
+            if (i%2 != 0){
+                System.out.println(i);
+            }
+            i++;
+            }
+        reader.close();
+    }
+}
